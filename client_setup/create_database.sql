1d7d8efb8b62a06c5e069f33070837934bac4877
Trever Ehrfurth <trever.ehrfurth@gmail.com>
2024-04-10 10:18:33 -0500

    update

--- client_setup/create_database.sql
+++ client_setup/create_database.sql
@@ -1,4 +1,5 @@
 -- Creates standardized databases for new client environments
+    USE WAREHOUSE COMPUTE_WH;
     CREATE DATABASE STAGE_DB;
     CREATE DATABASE STAGE_DB_DEV;
     CREATE DATABASE ETL_DB;
