7d16eddfe57950ce9a15900a7ae74d4cc7a351d1
Trever Ehrfurth <trever.ehrfurth@gmail.com>
2024-07-17 13:30:59 -0500

    Update auth policies to better standards

--- admin/Security/auth-policy.sql
+++ admin/Security/auth-policy.sql
@@ -1,17 +1,15 @@
--- Policies located under SECURITY.POLICIES database and schema in Snowflake
-
--- Policy to only accept SSO login + Require MFA
+-- Policy to only accept SSO login - MFA Requirement disabled as we require MFA from Azure for SSO
 CREATE OR REPLACE AUTHENTICATION POLICY user_auth_policy
   AUTHENTICATION_METHODS = ('SAML') -- 'PASSWORD'
   CLIENT_TYPES = ('SNOWFLAKE_UI', 'SNOWSQL', 'DRIVERS')
-  MFA_AUTHENTICATION_METHODS = ('SAML') -- 'PASSWORD'
-  MFA_ENROLLMENT = REQUIRED;
+  MFA_AUTHENTICATION_METHODS = ('SAML'); -- 'PASSWORD'
+  --MFA_ENROLLMENT = REQUIRED; -- Disable if using SSO/SCIM with MFA
 
 -- Policy to prevent lockout to admin allowing password access if SSO Fails
 CREATE OR REPLACE AUTHENTICATION POLICY admin_auth_policy
   AUTHENTICATION_METHODS = ('SAML', 'PASSWORD') -- 'PASSWORD'
   CLIENT_TYPES = ('SNOWFLAKE_UI', 'SNOWSQL', 'DRIVERS')
-  MFA_AUTHENTICATION_METHODS = ('PASSWORD', 'SAML') -- 'PASSWORD'
+  MFA_AUTHENTICATION_METHODS = ('PASSWORD', 'SAML')
   MFA_ENROLLMENT = REQUIRED;
 
 -- Policy for Service accounts to not require MFA
@@ -33,3 +31,14 @@ ALTER USER <username> SET AUTHENTICATION POLICY <policy_name>;
 -- Remove Authentication Policy
 ALTER ACCOUNT UNSET AUTHENTICATION POLICY;
 ALTER USER <username> UNSET AUTHENTICATION POLICY;
+
+--Unset users passwords once SSO enabled
+ALTER USER <username> UNSET PASSWORD;
+
+-- Check which authorization polcies are applied to who
+SELECT *
+FROM TABLE(
+    SECURITY.INFORMATION_SCHEMA.POLICY_REFERENCES(
+      POLICY_NAME => 'SECURITY.POLICIES.SERVICE_AUTH_POLICY'
+  )
+);
