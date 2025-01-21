20127c0bdbf3eceb5c6c0e221f1492e2b694c280
Trever Ehrfurth <trever.ehrfurth@gmail.com>
2025-01-20 18:25:00 -0600

    add client_mfa_auth_policy and update checks for April 2025 changes

--- client_setup/client_mfa_auth_policy.sql
+++ client_setup/client_mfa_auth_policy.sql
@@ -1,29 +1,28 @@
--- Policy to only accept SSO login - MFA Requirement disabled as we require MFA from Azure for SSO
-CREATE OR REPLACE AUTHENTICATION POLICY user_auth_policy
-  AUTHENTICATION_METHODS = ('SAML', 'KEYPAIR') -- 'PASSWORD'
+-- Policy to only accept SSO login - MFA Requirement disabled if IdP has MFA enabled
+CREATE OR REPLACE AUTHENTICATION POLICY sso_auth_policy
+  AUTHENTICATION_METHODS = ('SAML', 'KEYPAIR') -- 'PASSWORD' 
   CLIENT_TYPES = ('SNOWFLAKE_UI', 'SNOWSQL', 'DRIVERS')
   MFA_AUTHENTICATION_METHODS = ('SAML'); -- 'PASSWORD'
-  --MFA_ENROLLMENT = REQUIRED; -- Disable if using SSO/SCIM with MFA
+  --MFA_ENROLLMENT = REQUIRED; -- Disable if using SSO with MFA
 
 -- Policy to prevent lockout to admin allowing password access if SSO Fails
 CREATE OR REPLACE AUTHENTICATION POLICY admin_auth_policy
-  AUTHENTICATION_METHODS = ('SAML', 'PASSWORD', 'KEYPAIR') -- 'PASSWORD'
+  AUTHENTICATION_METHODS = ('SAML', 'PASSWORD', 'KEYPAIR')
   CLIENT_TYPES = ('SNOWFLAKE_UI', 'SNOWSQL', 'DRIVERS')
   MFA_AUTHENTICATION_METHODS = ('PASSWORD', 'SAML')
   MFA_ENROLLMENT = REQUIRED;
 
--- Policy for Service accounts to not require MFA
-CREATE OR REPLACE AUTHENTICATION POLICY service_auth_policy
-  AUTHENTICATION_METHODS = ('PASSWORD','KEYPAIR')
-  CLIENT_TYPES = ('SNOWFLAKE_UI', 'SNOWSQL', 'DRIVERS')
-  MFA_AUTHENTICATION_METHODS = ('PASSWORD');
-
--- Policy for Clients
-CREATE OR REPLACE AUTHENTICATION POLICY client_auth_policy
+-- Policy for External users outside your organization
+CREATE OR REPLACE AUTHENTICATION POLICY external_auth_policy
   AUTHENTICATION_METHODS = ('PASSWORD','KEYPAIR')
   CLIENT_TYPES = ('SNOWFLAKE_UI', 'SNOWSQL', 'DRIVERS')
   MFA_AUTHENTICATION_METHODS = ('PASSWORD')
   MFA_ENROLLMENT = REQUIRED;
+
+
+-- Service accounts should have their TYPE updated to 'SERVICE'
+ALTER <user> SET TYPE = 'SERVICE'; -- This will remove any stored passwords and break any integrations using such and force KeyPair
+
 
 -- Show authentication policies
 SHOW AUTHENTICATION POLICIES;
@@ -49,3 +48,13 @@ FROM TABLE(
       POLICY_NAME => 'SECURITY.POLICIES.SERVICE_AUTH_POLICY'
   )
 );
+
+-- Show users who will be affected by Snowflakes April 2025 MFA Requirements
+SELECT name, type, disabled, has_mfa, has_password
+FROM snowflake.account_usage.users
+WHERE deleted_on IS NULL
+  AND has_mfa = false
+  AND has_password
+  AND (type IS NULL OR type IN ('PERSON', 'LEGACY_SERVICE'))
+  AND disabled = 'false' -- optional: adjust for use case
+ORDER BY NAME ASC;
